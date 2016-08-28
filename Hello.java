0e80577c78accfb0cf9889b3973c60f70c3085d9
jinli <979451830@qq.com>
2016-08-28 18:29:09 +0800

    Modfied Hello.java File

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello{
 	public static void main(String args[]){
 		System.out.println("www.mldn.cn");
-		
-		
+		System.out.println("AAAAAAAA");
+		System.out.println("bbbbbbbb");
 	}
 }
